bd975f6082fd3b342de3159d4f1cc0848ac8055b
vrampal <vincent.rampal@amadeus.com>
2018-01-08 16:52:28 +0100

    Fixed indent

--- vars/paralUtils.groovy
+++ vars/paralUtils.groovy
@@ -4,25 +4,25 @@ import org.apache.tools.ant.DirectoryScanner;
 //
 // Partition an array into X parts of ~even size 
 //
-def partition(arrayList, nodes){
+def partition(arrayList, nodes) {
     def maxNodes = nodes.size();
-    if (arrayList.size() < maxNodes){
-	maxNodes = arrayList.size();
+    if (arrayList.size() < maxNodes) {
+        maxNodes = arrayList.size();
     }
     Map<String, List> resList = new HashMap<String, List>(maxNodes);
     def n = 0;
-    for (n=0; n < maxNodes; n++){
-	List nodeList = new  ArrayList();
-	resList.put(nodes[n], nodeList);
+    for (n = 0; n < maxNodes; n++) {
+        List nodeList = new ArrayList();
+        resList.put(nodes[n], nodeList);
     }
-    
-    n = 0;//reinit node count
+
+    n = 0;// reinit node count
     for (int i = 0; i < arrayList.size(); i++) {
-	if (n == maxNodes){
-	    n = 0;
-	}
-	resList.get(nodes[n]).add(arrayList[i]);
-	n++;
+        if (n == maxNodes) {
+            n = 0;
+        }
+        resList.get(nodes[n]).add(arrayList[i]);
+        n++;
     }
     return resList;
 }
@@ -30,55 +30,53 @@ def partition(arrayList, nodes){
 //
 // Get all Test cases given a Maven regexp, from a given path dir. 
 //
-def getTestsByRegexp(String path, String regexp){
-   DirectoryScanner scanner = new DirectoryScanner();
+def getTestsByRegexp(String path, String regexp) {
+    DirectoryScanner scanner = new DirectoryScanner();
     if (regexp != null) {
-	  log("Input provided: "+regexp);
+        log("Input provided: " + regexp);
         def rules = regexp.split(",");
         def fullRules = new String[rules.size()];
         def i = 0
-       
-        rules.each{
+
+        rules.each {
             def prefix = "**/"
             def suffix = "*"
             def rule = ""
-            it = it.trim(); //to remove any ' ' between rules if any
-            if (it.startsWith(prefix)){
+            it = it.trim(); // to remove any ' ' between rules if any
+            if (it.startsWith(prefix)) {
                 rule = it;
-            }
-            else {
+            } else {
                 rule = prefix + it;
             }
-            
-            if (! it.endsWith(suffix)) {
+
+            if (!it.endsWith(suffix)) {
                 rule += suffix;
             }
-            
-            //else  NOthing  fullRules[i++] = it; 
-	    //  }	    
-	  
-	    fullRules[i++] = rule 
+
+            // else  NOthing  fullRules[i++] = it;
+            // }
+
+            fullRules[i++] = rule
         }
-	   log ("Rules: $fullRules");
+        log("Rules: $fullRules");
         scanner.setIncludes(fullRules);
-    }
-    else {
+    } else {
         println("NO input provided, defaulting to: Test*");
         scanner.setIncludes("Test*".split(","));
     }
     scanner.setBasedir(path);
     scanner.setCaseSensitive(false);
     scanner.scan();
-    
+
     String[] files = scanner.getIncludedFiles();
     String[] testCases = new String[files.length];
-    def i =0;
+    def i = 0;
     files.each {
-	def file = it.replace("/", ".").replace("\\", ".");
-	file = file.replace(".groovy", "");
-	file = file.substring(file.lastIndexOf('.')+1);
-	log( "FILE: $file");
-	testCases[i++] = file;
+        def file = it.replace("/", ".").replace("\\", ".");
+        file = file.replace(".groovy", "");
+        file = file.substring(file.lastIndexOf('.') + 1);
+        log("FILE: $file");
+        testCases[i++] = file;
     }
     return testCases;
 }
@@ -86,25 +84,25 @@ def getTestsByRegexp(String path, String regexp){
 //
 // Search nodes where to build on, at least 1 idle exec...
 // 
-def getNodeList(){
+def getNodeList() {
     List<String> nodeList = new ArrayList<String>(3);
     def nodes = Jenkins.instance.getLabel("ANRJENKINS").getNodes();
-    nodes.each{
-    	log("Idle Exec: for $it :  " + it.computer.countIdle());
-	if (it.computer.countIdle() > 0) nodeList.add(it.displayName);
-    }    
+    nodes.each {
+        log("Idle Exec: for $it :  " + it.computer.countIdle());
+        if (it.computer.countIdle() > 0) nodeList.add(it.displayName);
+    }
     return nodeList;
 }
 
 //
 // Split the tests given regexp and the number of slaves 
 // 
-def splitTests(String path, String regexp) throws Exception{
-    String[] files =  getTestsByRegexp(path, regexp);
-    if ((files == null) || (files.size() == 0)){
-	throw new Exception("No tests matching regexp: " + regexp);
+def splitTests(String path, String regexp) throws Exception {
+    String[] files = getTestsByRegexp(path, regexp);
+    if ((files == null) || (files.size() == 0)) {
+        throw new Exception("No tests matching regexp: " + regexp);
     }
-    log("--There are " + files.size() + " tests matching regexp. List: \n " + files);    
+    log("--There are " + files.size() + " tests matching regexp. List: \n " + files);
     def nodeList = getNodeList();
     log("---Found $nodeList slaves to run parallel tests");
     def resList = partition(files, nodeList);
@@ -115,30 +113,30 @@ def splitTests(String path, String regexp) throws Exception{
 //
 // Manage the parallelization of tests 
 //
-def parallelize_tests(splits, type, inclusionsFile, results, prepare, run){
+def parallelize_tests(splits, type, inclusionsFile, results, prepare, run) {
     log("SPLITS: $splits");
     def branches = [:];
-    splits.each{ nodeName, tests ->
-	    log( "nodeName: $nodeName ==> tests: $tests");
-	branches["${type}_${nodeName}"] ={
-	    stage("${type}_${nodeName}") {
-		node (nodeName) {
-		    // prepare tests for this node
-		    prepare(); 
-		    writeFile file: inclusionsFile, text: tests.join("\n");
-		    //		    if (debugMode){
-		    //sh "cat inclusions.txt";
-		    //}
-		    //run tests for this node
-		    run();
-		    
-		    //publish junits for this node
-		    if (results != null){
-		        junit results;
-		    }
-		}
-	    }
-	}	    
+    splits.each { nodeName, tests ->
+        log("nodeName: $nodeName ==> tests: $tests");
+        branches["${type}_${nodeName}"] = {
+            stage("${type}_${nodeName}") {
+                node(nodeName) {
+                    // prepare tests for this node
+                    prepare();
+                    writeFile file: inclusionsFile, text: tests.join("\n");
+                    // if (debugMode){
+                    // sh "cat inclusions.txt";
+                    // }
+                    //run tests for this node
+                    run();
+
+                    //publish junits for this node
+                    if (results != null) {
+                        junit results;
+                    }
+                }
+            }
+        }
     }
     parallel branches
 }
